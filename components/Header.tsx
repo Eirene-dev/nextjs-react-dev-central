2abd035c004eb4bedc976449e58e541642f59334
Eirene <zu007kr@gmail.com>
2023-09-07 00:44:13 +0900

    Logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,6 +5,7 @@ import Link from './Link'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
 import SearchButton from './SearchButton'
+import Image from 'next/image'
 
 const Header = () => {
   return (
@@ -13,10 +14,10 @@ const Header = () => {
         <Link href="/" aria-label={siteMetadata.headerTitle}>
           <div className="flex items-center justify-between">
             <div className="mr-3">
-              <Logo />
+              <Image src="/static/Logo_RNC.png" alt="ReactNextCentral" width={65} height={65} />
             </div>
             {typeof siteMetadata.headerTitle === 'string' ? (
-              <div className="hidden h-6 text-2xl font-semibold sm:block">
+              <div className="h-6 text-2xl font-semibold">
                 {siteMetadata.headerTitle}
               </div>
             ) : (
@@ -25,14 +26,14 @@ const Header = () => {
           </div>
         </Link>
       </div>
-      <div className="flex items-center leading-5 space-x-4 sm:space-x-6">
+      <div className="flex items-center space-x-4 leading-5 sm:space-x-6">
         {headerNavLinks
           .filter((link) => link.href !== '/')
           .map((link) => (
             <Link
               key={link.title}
               href={link.href}
-              className="hidden sm:block font-medium text-gray-900 dark:text-gray-100"
+              className="hidden font-medium text-gray-900 sm:block dark:text-gray-100"
             >
               {link.title}
             </Link>
