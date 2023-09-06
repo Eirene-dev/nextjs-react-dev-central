6e45fa3a6ccd86206ef6981df77c12cfd5d2a91c
Eirene <zu007kr@gmail.com>
2023-09-07 07:54:12 +0900

    docs

--- app/docs/[[...slug]]/layout.tsx
+++ app/docs/[[...slug]]/layout.tsx
@@ -26,11 +26,11 @@ export default function DocsLayout({ children }: DocsLayoutProps) {
   return (
     <div className="flex-1 md:grid md:grid-cols-[190px_1fr] md:gap-6 lg:grid-cols-[200px_1fr] lg:gap-10">
       <button className="md:hidden" onClick={onToggleNav}>
-        [> 문서 목차]
+        문서 목차
       </button>
       <aside
         style={{ display: navShow ? 'block' : 'none' }}
-        className="fixed top-14 z-30 h-[calc(100vh-3.5rem)] w-full shrink-0 overflow-y-auto border-r py-6 pr-2 bg-white bg-opacity-90 dark:bg-gray-950 md:sticky md:block lg:py-10 md:hidden"
+        className="fixed top-30 z-30 h-[calc(100vh-3.5rem)] w-full shrink-0 overflow-y-auto border-r py-6 pr-2 bg-white bg-opacity-90 dark:bg-gray-950 md:sticky md:block lg:py-10 md:hidden"
       >
         <DocsSidebarNav items={docsConfig.sidebarNav} />
       </aside>
