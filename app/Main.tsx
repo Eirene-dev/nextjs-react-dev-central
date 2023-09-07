6591d1a8230c7d46ae868046b3df4cbcd1491381
Eirene <zu007kr@gmail.com>
2023-09-07 16:26:39 +0900

    illustation

--- app/Main.tsx
+++ app/Main.tsx
@@ -72,7 +72,7 @@ export default function Main({ posts }) {
           </p>
         </div>
       </section>
-      <section id="blog" className="container py-8 md:py-12 lg:py-24">
+      <section id="blog" className="container items-center py-8 md:py-12 lg:py-24">
         <div className="mx-auto flex max-w-[58rem] flex-col items-center justify-center gap-4 text-center">
           <h2 className="font-heading text-3xl leading-[1.1] sm:text-3xl md:text-6xl">
             블로그 태그
