54245523b6dffb49d82d3b261a24a462e45250a0
Eirene <zu007kr@gmail.com>
2023-09-15 11:54:33 +0900

    doc next.js

--- components/callout.tsx
+++ components/callout.tsx
@@ -9,7 +9,7 @@ interface CalloutProps {
 export function Callout({ children, icon, type = 'default', ...props }: CalloutProps) {
   return (
     <div
-      className={cn('my-6 flex items-start rounded-md border border-l-4 p-4', {
+      className={cn('my-6 items-start rounded-md border border-l-4 p-4 overflow-hidden', {
         'border-red-900 bg-red-50': type === 'danger',
         'border-yellow-900 bg-yellow-50': type === 'warning',
       })}
