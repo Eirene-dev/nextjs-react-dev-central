b71e62f646ade547a61d13be7da4f9582bfe7ed1
Eirene <zu007kr@gmail.com>
2023-09-15 13:56:18 +0900

    callout for page and app

--- components/callout.tsx
+++ components/callout.tsx
@@ -12,8 +12,8 @@ export function Callout({ children, icon, type = 'default', ...props }: CalloutP
       className={cn('my-6 items-start border p-4 overflow-hidden shadow-md', {
         'border-red-900 bg-red-50 border-l-4 rounded-md': type === 'danger',
         'border-yellow-900 bg-yellow-50 border-l-4 rounded-md': type === 'warning',
-        'border-dashed border-gray-200 bg-orange-50': type === 'page',   
-        'border-dashed border-gray-200 bg-green-50': type === 'app', 
+        'border-dashed border-gray-200 bg-orange-50': type === 'page',
+        'border-dashed border-gray-200 bg-green-50': type === 'app',
       })}
       {...props}
     >
