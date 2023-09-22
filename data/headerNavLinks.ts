4ce618a8f55fda867c9f2e07f9b79d13e811d706
Eirene <zu007kr@gmail.com>
2023-09-22 15:30:58 +0900

    doc issue

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,7 +2,7 @@ const headerNavLinks = [
   { href: '/', title: '홈' },
   { href: '/blog', title: '블로그' },
   { href: '/tags', title: '태그' },
-  { href: '/docs', title: '문서' },
+  { href: '/docs/getting-started', title: '문서' },
   { href: '/example', title: '예제' },
   // { href: '/about', title: 'About' },
 ]
