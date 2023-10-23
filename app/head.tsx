edb786f68f59452c6771cb665b98efb4c503602b
Eirene <zu007kr@gmail.com>
2023-10-23 16:02:29 +0900

    google adsense

--- app/head.tsx
+++ app/head.tsx
@@ -1,3 +1,5 @@
+import Script from 'next/script'
+
 export default function Head() {
   return (
     <>
@@ -7,22 +9,19 @@ export default function Head() {
       <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png" />
       <link rel="manifest" href="/static/favicons/site.webmanifest" />
       <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#5bbad5" />
-      <script
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1194474024149121"
-        crossOrigin="anonymous"
-      ></script>
       <meta name="google-adsense-account" content="ca-pub-1194474024149121"></meta>
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
       <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <script
+      <Script
+        id="script-component-ad"
+        data-ad-client="ca-pub-1194474024149121"
         async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1194474024149121"
-        crossOrigin="anonymous"
-      ></script>
+        strategy="lazyOnload"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+      />
     </>
   )
 }
